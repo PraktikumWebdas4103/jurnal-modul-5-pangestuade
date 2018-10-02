d1e1c74bbb79cdc70bd507653a34818fa9ab527f
Ade Pangestu <43510254+pangestuade@users.noreply.github.com>
2018-10-02 16:57:36 +0700

    Update prosesregris.php

--- prosesregris.php
+++ prosesregris.php
@@ -1,4 +1,27 @@
+
 <?php
 
+include("koneksi.php");
+if(isset($_POST['submit'])){
+
+    $nim = $_POST['nim'];
+    $nama = $_POST['nama'];
+    $email = $_POST['email'];
+    
+
+    $sql = "INSERT INTO registrasi (nim, nama, email) VALUE ('$nim','$nama', '$email')";
+    $query = mysqli_query($koneksi, $sql);
+
+    if( $query ) {
+   
+        header('Location: prosesregis.php');
+        echo "Data disimpan ke database";
+    } else {
+       
+        echo "gagal";
+    }
+
+
+} 
 
 ?>
