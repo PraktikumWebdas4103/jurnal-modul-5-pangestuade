4ef6c9c49de3f252f8dfb0aca1fbbcd8ba202bd1
Ade Pangestu <43510254+pangestuade@users.noreply.github.com>
2018-10-02 16:56:52 +0700

    Update komentar.php

--- komentar.php
+++ komentar.php
@@ -1,3 +1,18 @@
-<?php
-
+<form action=" " method="GET">
+	<center>
+	Komentar : <input type="text" name="kalimat" style="width: 15%; height: 10%; color: black; background-color: lightgrey; border-radius: 5px">
+	<input type="submit" name="submit" value="Cari">
+	</center>
+</form>
+	<?php
+		if(isset($_GET["submit"])) {
+			$kal=$_GET['kalimat'];
+			$jumlahKata= str_word_count($kal);
+			$jumlahParagraf= substr_count($kal, ".");
+			$jumlahKarakterDanSpasi= strlen($kal);
+				echo "Output :<br><br>";
+				echo "Jumlah kata : $jumlahKata <br>";
+				echo "Jumlah paragraf : $jumlahParagraf <br>";
+				echo "Jumlah karakter dan spasi : $jumlahKarakterDanSpasi <br>";
+ }
 ?>
